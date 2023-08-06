f48fdbce04741f0a5b9cff37795a28f6b4df33ca
Raphael Santos <rvsantos97@gmail.com>
2023-08-06 02:19:15 -0300

    refactor: step 1 - switching style for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "goblin-green": "#0A9E85",
+      "discord-blue": "#7289DA",
+      "gray-ish": "#E2E3DE",
+      white: "#fff",
+      black: "#000",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        satisfy: ["'Satisfy'"],
+      },
+    },
+  },
+  plugins: [],
+};
